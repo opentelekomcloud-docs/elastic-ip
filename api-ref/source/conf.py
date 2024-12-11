d00a6608d88ae7306da2e6424e9d390fc24e88db
SebastianGode <70581801+SebastianGode@users.noreply.github.com>
2024-12-11 10:49:54 +0100

    Update tox.ini && conf.py file (#40)
    Update tox.ini && conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: Tino Schr

--- api-ref/source/conf.py
+++ api-ref/source/conf.py
@@ -110,7 +110,7 @@ html_copy_source = False
 latex_documents = [
     ('index',
      'eip-api-ref.tex',
-     u'Elastic IP - API Reference',
+    u'Elastic IP - API Reference',
      u'OpenTelekomCloud', 'manual'),
 ]
 
